58a8241d5e15214ef7e7512ea9198d307829c04d
TeiturMcSwagger <teiturargeseloy@hotmail.com>
2018-04-14 10:41:16 +0200

    Det virker næsten
    det mangler at lave et point system og at få batene til at bevege sig seperat af hinanden

---
+++ PongMaster/Assets/Ball.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float speed = 30;
+
+    // Use this for initialization
+
+    void Start () {
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // Hit the left Racket?
+        if (col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "RacketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
+    {
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+	
+}
