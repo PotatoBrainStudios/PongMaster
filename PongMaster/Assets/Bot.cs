5e9fecf5af4454e65384633518527127766f4e17
TeiturMcSwagger <teiturargeseloy@hotmail.com>
2018-04-14 15:29:01 +0200

    bot

---
+++ PongMaster/Assets/Bot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour {
+    Transform bar;
+
+	// Use this for initialization
+	void Start () {
+        bar = GameObject.Find("Ball").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2(bar.position.x, bar.position.y);
+	}
+}
