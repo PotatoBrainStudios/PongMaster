7aa9f51214fc367b2007f7fcea05e1d0bb9a2822
TeiturMcSwagger <teiturargeseloy@hotmail.com>
2018-04-14 16:17:31 +0200

    pointsystem

---
+++ PongMaster/Assets/PointSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSystem : MonoBehaviour {
+
+    int playerLeft = 0;
+    int playerRight = 0;
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "WallLeft")
+        {
+            playerRight += 1;
+            Debug.Log("+1 right");
+        }
+        if (col.gameObject.name == "WallRight")
+        {
+            playerLeft += 1;
+            Debug.Log("+1 left");
+        }
+    }
+}
