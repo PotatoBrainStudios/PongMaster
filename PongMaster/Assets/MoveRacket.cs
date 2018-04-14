cf0256d3926d18fae65b294746dccabe78ddd7e8
Lau Kondrup <lau.kondrup@gmail.com>
2018-04-14 13:47:16 +0200

    Touch changes paddles' velocity

--- PongMaster/Assets/MoveRacket.cs
+++ PongMaster/Assets/MoveRacket.cs
@@ -7,9 +7,51 @@ public class MoveRacket : MonoBehaviour
     public float speed = 30;
     public string axis = "Vertical";
 
+    private Vector3 firstTouchPos;   //First touch position
+    private Vector3 lastTouchPos;   //Last touch position
+    private float dragDistance;  //minimum distance for a swipe to be registered
+
+    void Start()
+    {
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+    }
     void FixedUpdate()
     {
         float v = Input.GetAxisRaw(axis);
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+        // GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                firstTouchPos = touch.position;
+                lastTouchPos = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lastTouchPos = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lastTouchPos = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lastTouchPos.y - firstTouchPos.y) > dragDistance)
+                {//It's a drag
+                    if (lastTouchPos.y > firstTouchPos.y)  //If the movement was up
+                    {   //Up swipe
+                        Debug.Log("Up Swipe");
+                        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+                    }
+                    else
+                    {   //Down swipe
+                        Debug.Log("Down Swipe");
+                        GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+
+                    }
+                }
+            }
+        }
     }
 }
